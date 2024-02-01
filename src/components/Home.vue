4d89fd5b3b7e282c06d01d361a36fd54555eaefa
mao <mauricio.1020@hotmail.com>
2024-02-01 06:05:58 -0500

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {onMounted} from "vue";
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+
+onMounted(async () => {
+    await authStore.getUser();
+})
+</script>
+<template>
+    <div v-if="authStore.user">
+        <h1>{{ authStore.user.name }}</h1>
+        <h1>{{ authStore.user.email }}</h1>
+    </div>
+    <div v-else>
+        <h1>Go an login</h1>
+    </div>
+</template>
